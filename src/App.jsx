be19cf7e680f5292a9550b1913ed4f7e7684a3eb
Godstime Osarobo <nosarobo@gmail.com>
2024-02-25 22:17:26 +0000

    Codecast Website

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div style={
+            {
+                textAlign: 'center',
+                width: '100vw',
+                height: '100vh'
+            }
+        }>
+            <h1>Welcome to CodeCasts!</h1>
+        </div>
+    )
+}
+
+export default App;
